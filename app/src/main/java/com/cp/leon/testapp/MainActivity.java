788593c5b297776f432f71192d12347ca8450647
gintamaGG <1012575033@qq.com>
2017-05-18 17:21:28 +0800

    first try

---
+++ app/src/main/java/com/cp/leon/testapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cp.leon.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
